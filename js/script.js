457f87ad2dca65c65a3dde73c511f17927afcaee
JuanFerrer <juanemilioferrer@outlook.com>
2017-04-26 20:19:25 +0100

    Fixed scrollbar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // On hover open and close sidebar menu
 function openSidebar() {
-    document.getElementById("sidebar").style.width = "25vw";
+    document.getElementById("sidebar").style.width = "250px";
 }
 
 function closeSidebar() {
